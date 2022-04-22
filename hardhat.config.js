21017f0ece6ab6d8d796780bd529eb78355572ab
Kentaro Kuribayashi <kentarok@gmail.com>
2022-04-23 00:53:31 +0900

    https://hardhat.org/tutorial/deploying-to-a-live-network.html

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,18 @@
+require('dotenv').config();
 require("@nomiclabs/hardhat-waffle");
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.13",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${ROPSTEN_PRIVATE_KEY}`]
+    }
+  }
 };
